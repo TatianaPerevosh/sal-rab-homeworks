177bce0e98524677ce6862bd8ce4c12d45a5d6d7
TatianaPerevosh <perevoshikova-ta@mail.ru>
2022-12-30 20:41:15 +0400

    finish2 commit

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,16 +34,16 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-            
-    let data = {client, order: {address, sum}, goods: [title, count]};
+    
+    let data = {client:String, order: {address, sum}, goods: []};
 
-    let countOfGoods = goods.length8 - 1;
+    let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    data.client = name + " "+phone;
+    data.client = name + " " + phone;
 
     data.order.address =  "ул. " + address.street + ", дом " + address.house + ", "+ address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
     data.order.sum = sum;
